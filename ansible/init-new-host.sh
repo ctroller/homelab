48bfafb81e175fbff70950faf842bdfd8087b19e
Christian Troller <ctroller@proton.me>
2024-03-13 20:09:58 +0100

    some tweaks, phpipam config

--- ansible/init-new-host.sh
+++ ansible/init-new-host.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-ansible-playbook playbooks/monitoring/agents.yml
+ansible-playbook playbooks/apt.yml
 ansible-playbook playbooks/docker.yml
+ansible-playbook playbooks/monitoring/agents.yml
 ansible-playbook playbooks/monitoring/promconfig.yml
